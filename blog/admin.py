ae43791f13e25bcbce422107c791a7ee9f5b23b7
gleb165 <83755952+gleb165@users.noreply.github.com>
2025-01-28 21:51:59 +0200

    first commit

---
+++ blog/admin.py
@@ -0,0 +1,26 @@
+from django.contrib import admin
+from .models import TurnoverDocument, document_types
+# Register your models here.
+
+
+
+@admin.register(document_types)
+class document_typesAdmin(admin.ModelAdmin):
+    list_display = ['description']
+
+
+
+@admin.register(TurnoverDocument)
+class TurnoverDocumentAdmin(admin.ModelAdmin):
+    list_display = ['title', 'description', 'department', 'publish',
+                    'document_type','document_file',
+                    'filename', 'created_at', 'updated_at']
+    list_filter = ['status', 'created_at', 'publish', 'auther', 'updated_at']
+    search_fields = ['title', 'description']
+    prepopulated_fields = {'slug': ('title',)}
+    raw_id_fields = ['auther']
+    date_hierarchy = 'publish'
+    ordering = ['status', 'publish', 'updated_at']
+    show_facets = admin.ShowFacets.ALWAYS
+
+    
---
+++ blog/models.py
@@ -0,0 +1,48 @@
+from django.db import models
+from django.conf import settings
+from django.utils import timezone
+import os
+# Create your models here.
+
+
+class document_types(models.Model):
+    type_name = models.CharField(max_length=250)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.type_name
+
+
+class TurnoverDocument(models.Model):
+
+    class Status(models.TextChoices):
+        DRAFT = ('DF', 'Draft')
+        UnderReview = ('UR', 'UnderReview')
+        Approved = ('A', 'Approved')
+        Rejected = ('R', 'Rejected')
+
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    auther = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='document_post')
+    department = models.CharField(max_length=255) #?????????
+    publish = models.DateTimeField(default=timezone.now)
+    document_type = models.ForeignKey(document_types, on_delete=models.CASCADE)
+    document_file = models.FileField(upload_to='turnover_documents/')
+    slug = models.SlugField(max_length=250)
+    status = models.CharField(
+        max_length=2,
+        choices=Status,
+        default=Status.DRAFT
+    )
+
+    class Meta:
+        ordering = ['-publish']
+        indexes = [models.Index(fields=['-publish'])]
+
+    def filename(self):
+        return os.path.basename(self.document_file.name)
+
+    def __str__(self):
+        return self.title
