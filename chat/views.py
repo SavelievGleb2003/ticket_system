d3c1ad52d89dad1e1601383f6be39c08c3aed8e6
SavelievGleb2003 <savelievglib2003@gmail.com>
2025-02-27 15:01:31 +0200

    for home

--- chat/views.py
+++ chat/views.py
@@ -7,6 +7,7 @@ from django.contrib.auth.decorators import login_required
 from .models import Chat, ChatMessage
 from tickets.models import Ticket
 from django.http import HttpResponseForbidden
+from django.db.models import Q
 
 @login_required
 def send_message(request, chat_id):
@@ -34,10 +35,13 @@ def chat_detail(request, ticket_id):
 
     # Retrieve all messages for the chat (ordered by timestamp)
     messages = ChatMessage.objects.filter(chat=chat).order_by('timestamp')
-
+    chats = Chat.objects.filter(
+        Q(ticket__accepted_by=request.user) | Q(ticket__created_by=request.user)
+    )
     return render(request, 'chat/chat_detail.html', {
         'ticket': ticket,
         'chat': chat,
         'messages': messages,  # pass messages to template
         'has_access': has_access,  # <-- передаем в шаблон
+        'tickets': chats
     })
